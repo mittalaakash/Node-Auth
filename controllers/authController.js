8d62e8abe129335ccda8477ae6025a38a0d35f76
Mridul <mit.ash.619@gmail.com>
2023-04-14 03:53:56 +0530

    auth controller finding user

---
+++ controllers/authController.js
@@ -0,0 +1,20 @@
+const User = require('../model/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const handleLogin = async (req, res) => {
+  const cookies = req.cookies;
+
+  const { user, pwd } = req.body;
+  if (!user || !pwd)
+    return res
+      .status(400)
+      .json({ message: 'Username and password are required.' });
+
+  const foundUser = await User.findOne({ username: user }).exec();
+  if (!foundUser) return res.sendStatus(401); //Unauthorized
+  // evaluate password
+  const match = await bcrypt.compare(pwd, foundUser.password);
+};
+
+module.exports = { handleLogin };
