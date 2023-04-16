a7eaee7b037ffead8e59c38e390a87bef82e98c5
Mridul <mit.ash.619@gmail.com>
2023-04-17 03:02:16 +0530

    logout controller

---
+++ controllers/logoutController.js
@@ -0,0 +1,11 @@
+const User = require('../model/User');
+
+const handleLogout = async (req, res) => {
+  // On client, also delete the accessToken
+
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204); //No content
+  const refreshToken = cookies.jwt;
+};
+
+module.exports = { handleLogout };
