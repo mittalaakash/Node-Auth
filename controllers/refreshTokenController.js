9565f9ceeea2a2d0b8a6da2b359315d82ee8ae23
Mridul <mit.ash.619@gmail.com>
2023-04-16 08:53:57 +0530

    evaluate refresh token

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -30,6 +30,53 @@ const handleRefreshToken = async (req, res) => {
   const newRefreshTokenArray = foundUser.refreshToken.filter(
     rt => rt !== refreshToken,
   );
+
+  // evaluate jwt
+  jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET,
+    async (err, decoded) => {
+      if (err) {
+        // expired refresh token
+        foundUser.refreshToken = [...newRefreshTokenArray];
+        const result = await foundUser.save();
+      }
+      if (err || foundUser.username !== decoded.username)
+        return res.sendStatus(403);
+
+      // Refresh token was still valid
+      const roles = Object.values(foundUser.roles);
+      const accessToken = jwt.sign(
+        {
+          UserInfo: {
+            username: decoded.username,
+            roles: roles,
+          },
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '10s' },
+      );
+
+      const newRefreshToken = jwt.sign(
+        { username: foundUser.username },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: '15s' },
+      );
+      // Saving refreshToken with current user
+      foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
+      const result = await foundUser.save();
+
+      // Creates Secure Cookie with refresh token
+      res.cookie('jwt', newRefreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None',
+        maxAge: 24 * 60 * 60 * 1000,
+      });
+
+      res.json({ accessToken });
+    },
+  );
 };
 
 module.exports = { handleRefreshToken };
