7dd2ea45a3854e77ff20857727dee5289fcd67c9
Mridul <mit.ash.619@gmail.com>
2023-04-16 11:02:18 +0530

    detect refresh token reuse

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -8,6 +8,24 @@ const handleRefreshToken = async (req, res) => {
   res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
 
   const foundUser = await User.findOne({ refreshToken }).exec();
+
+  // Detected refresh token reuse!
+  if (!foundUser) {
+    jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+      async (err, decoded) => {
+        if (err) return res.sendStatus(403); //Forbidden
+        // Delete refresh tokens of hacked user
+        const hackedUser = await User.findOne({
+          username: decoded.username,
+        }).exec();
+        hackedUser.refreshToken = [];
+        const result = await hackedUser.save();
+      },
+    );
+    return res.sendStatus(403); //Forbidden
+  }
 };
 
 module.exports = { handleRefreshToken };
