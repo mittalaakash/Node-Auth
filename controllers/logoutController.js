ee4ea16b59dd7c40cbebc69ac17dc80ebfe52e2d
Mridul <mit.ash.619@gmail.com>
2023-04-17 22:35:38 +0530

    check refresh token in db

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -6,6 +6,13 @@ const handleLogout = async (req, res) => {
   const cookies = req.cookies;
   if (!cookies?.jwt) return res.sendStatus(204); //No content
   const refreshToken = cookies.jwt;
+
+  // Is refreshToken in db?
+  const foundUser = await User.findOne({ refreshToken }).exec();
+  if (!foundUser) {
+    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+    return res.sendStatus(204);
+  }
 };
 
 module.exports = { handleLogout };
