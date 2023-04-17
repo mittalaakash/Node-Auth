bfdeecae70342c1eedf132a9b7184c745608163e
Mridul <mit.ash.619@gmail.com>
2023-04-18 01:28:34 +0530

    create new employee controller

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -7,6 +7,26 @@ const getAllEmployees = async (req, res) => {
   res.json(employees);
 };
 
+const createNewEmployee = async (req, res) => {
+  if (!req?.body?.firstname || !req?.body?.lastname) {
+    return res
+      .status(400)
+      .json({ message: 'First and last names are required' });
+  }
+
+  try {
+    const result = await Employee.create({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+    });
+
+    res.status(201).json(result);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 module.exports = {
   getAllEmployees,
+  createNewEmployee,
 };
