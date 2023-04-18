11374c5c7c96e8e63dc63b4bd3da2e21e21dc567
Mridul <mit.ash.619@gmail.com>
2023-04-18 09:16:22 +0530

    route handler: get all employees

---
+++ routes/api/employees.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const employeesController = require('../../controllers/employeesController');
+const ROLES_LIST = require('../../config/roles_list');
+const verifyRoles = require('../../middleware/verifyRoles');
+
+router.route('/').get(employeesController.getAllEmployees);
+
+module.exports = router;
