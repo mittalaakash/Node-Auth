2fcbe4a8c338b373673a45ee478f64e4edb938b1
Mridul <mit.ash.619@gmail.com>
2023-04-22 20:38:40 +0530

    get use controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -19,7 +19,20 @@ const deleteUser = async (req, res) => {
   res.json(result);
 };
 
+const getUser = async (req, res) => {
+  if (!req?.params?.id)
+    return res.status(400).json({ message: 'User ID required' });
+  const user = await User.findOne({ _id: req.params.id }).exec();
+  if (!user) {
+    return res
+      .status(204)
+      .json({ message: `User ID ${req.params.id} not found` });
+  }
+  res.json(user);
+};
+
 module.exports = {
   getAllUsers,
   deleteUser,
+  getUser,
 };
